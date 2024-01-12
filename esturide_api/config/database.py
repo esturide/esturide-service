9824b26131b837bbeeefbdd5a532760446ce311c
alexToMars <javierreynosoqxk12@gmail.com>
2024-01-12 10:01:23 -0600

    Feature:Create user registration and login endpoints

---
+++ esturide_api/config/database.py
@@ -0,0 +1,22 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+sqlite_file_name="../database.sqlite"
+base_dir=os.path.dirname(os.path.realpath(__file__))
+
+database_url=f"sqlite:///{os.path.join(base_dir,sqlite_file_name)}"
+
+engine=create_engine(database_url,echo=True,connect_args={"check_same_thread":False})
+
+SessionLocal=sessionmaker(bind=engine)
+
+Base=declarative_base()
+
+def get_db():
+    db=SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
