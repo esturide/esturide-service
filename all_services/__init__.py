358d2df08081e8d6a0011a2edd3fe6fcf1c2283d
SealtielFreak <Die.Seal_Valderrama99@outlook.com>
2024-08-15 15:48:58 -0600

    Minor changes (Rewriting names in documentation)

--- all_services/__init__.py
+++ all_services/__init__.py
@@ -2,7 +2,10 @@ from fastapi import FastAPI
 
 from user_management_system import user_management_system_service
 
-main = FastAPI()
+main = FastAPI(
+    title="EstuRide API"
+)
+
 main.mount("/user_management_system", user_management_system_service)
 
 
