9a2717c14c5d6c96358b271657f643949034405e
SealtielFreak <Die.Seal_Valderrama99@outlook.com>
2024-10-14 11:50:53 -0600

    Implemented repository for user score

--- app/services/user_match_network_system/infrastructure/repository.py
+++ app/services/user_match_network_system/infrastructure/repository.py
@@ -2,12 +2,28 @@ from typing import Tuple
 
 from sqlalchemy.orm import Session
 
-from app.services.user_match_network_system.domain.models import Travel
+from app.services.user_match_network_system.domain.models import Travel, UserScore
 
 
 class UserScoreRepository:
     def __init__(self, db: Session):
         self.db = db
+
+    async def create(
+            self, passenger_id: int, driver_id: int, stars: int, comment: str
+    ) -> Tuple[bool, UserScore]:
+
+        user_score = UserScore(
+            passenger_id=passenger_id,
+            driver_id=driver_id,
+            stars=stars,
+            comment=comment,
+        )
+
+        self.db.add(user_score)
+        self.db.commit()
+
+        return True, user_score
 
 
 class TravelRepository:
