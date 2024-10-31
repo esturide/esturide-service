2b39c57a25214480d080d10b53f907bd6b93e831
SealtielFreak <Die.Seal_Valderrama99@outlook.com>
2024-10-31 01:10:21 -0600

    Dependency repair

--- app/services/user_management_system/oauth2.py
+++ app/services/user_management_system/oauth2.py
@@ -1,6 +1,7 @@
 from fastapi import HTTPException, status
 from app.shared.credentials import verify_access_token
 from app.shared.dependencies import OAuth2BearerDepend, DataBaseSession
+from app.shared.domain.models.user_management_system import User
 
 
 def get_current_user(
@@ -15,6 +16,6 @@ def get_current_user(
 
     token = verify_access_token(token, credentials_exception)
 
-    user = db.query(models.User).filter(models.User.id == token).first()
+    user = db.query(User).filter(User.id == token).first()
 
     return user
