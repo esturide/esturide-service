f0c60adc21ec95fcfa1aaab958db1bdf94a3fa99
SealtielFreak <Die.Seal_Valderrama99@outlook.com>
2024-10-31 11:21:21 -0600

    Fix imports

--- app/shared/domain/models/__init__.py
+++ app/shared/domain/models/__init__.py
@@ -1,2 +1,2 @@
 from app.shared.domain.models.user_management_system import User, Driver, Automobile, Passenger
-from app.shared.domain.models.user_match_network_system import UserScore, Travel, Ride
+from app.shared.domain.models.user_match_network_system import UserScore, TravelSchedule, Ride
